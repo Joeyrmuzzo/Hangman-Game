9d5607196e1be957be92714775b6aa63443e30d6
Joeyrmuzzo <57329326+Joeyrmuzzo@users.noreply.github.com>
2020-03-31 15:16:11 -0400

    Add files via upload

---
+++ course project/Program-week-7.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] word = "muzzo".ToCharArray();
+            char letter;
+            char[] guessed = new char[26];
+            int score = 0;
+            int index = 0;
+            bool found = false;
+            
+            Console.WriteLine("Welcome to the hangman game!");
+
+            
+            for (int guessCount = 0; guessCount <10 ; guessCount++)
+            {
+                for(int symbol=0;symbol<word.Length;symbol++)
+                {
+                    for(int correctlyGuessed=0;correctlyGuessed<index;correctlyGuessed++)
+                    {
+                        if (word[symbol] == guessed[correctlyGuessed])
+                            found = true;
+                    }
+                    if (found == true)
+                    {
+                        Console.Write(word[symbol]);
+                        found = false;
+                    }
+                    else
+                        Console.Write("*");
+                }
+                Console.WriteLine();
+                Console.Write("Enter a letter please: ");
+
+                letter = char.Parse(Console.ReadLine());
+                if (word.Contains(letter))
+                {
+                    Console.WriteLine("You guessed correctly!");
+                    guessed[index] = letter;
+                    index++;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed incorrectly.");
+                    score++;
+                }
+
+            }
+            Console.WriteLine();
+                Console.WriteLine("your score is: " + score);
+                Console.ReadLine();
+            
+        }
+    }
+}
---
+++ course project/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace course_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter1 = 'm', letter2 = 'u', letter3 = 'z', letter4 = 'z', letter5 = 'o', letter;
+            char[] guessed = new char[26];
+            int score = 0;
+            int index = 0;
+            
+            Console.WriteLine("Welcome to the hangman game!");
+
+            Console.WriteLine("Enter a letter please: ");
+            for (int guessCount = 0; guessCount <10 ; guessCount++)
+            {
+                Console.WriteLine("*****");
+                letter = char.Parse(Console.ReadLine());
+                if (letter == letter1 || letter == letter2 || letter == letter3 || letter == letter4 || letter == letter5)
+                {
+                    Console.WriteLine("You guessed correctly!");
+                    guessed[index] = letter;
+                    index++;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed incorrectly.");
+                    score++;
+                }
+
+            }
+                Console.WriteLine("your score is: " + score);
+                Console.ReadLine();
+            
+        }
+    }
+}
